74b657586b1d64ce5a94bd835a71a4c40269b992
Ludovic Muller <ludovicmuller1@gmail.com>
2023-12-05 15:42:52 +0100

    ci: fetch input can support multiple prefixes

--- scripts/fetch_input.sh
+++ scripts/fetch_input.sh
@@ -3,16 +3,34 @@
 # Make the script crash in case of issue
 set -eu
 
-# Configuration
-INPUT_DEST_NAME="input/input.json"
-FILE_PREFIX="StABS_records"
+# Load the file prefixes
+. ./scripts/file_prefix.sh
 
-# Get the source name of the last full input file
-INPUT_SOURCE_NAME=$(mc ls "bucket/${S3_BUCKET}/${S3_PATH}/${FILE_PREFIX}" | awk '{ print $NF }' | sort | tail -n1)
-if [ -z "${INPUT_SOURCE_NAME}" ]; then
-  echo "ERROR: No input file was found that has this prefix '${FILE_PREFIX}'" >&2
-  exit 1
-fi
-mc cp "bucket/${S3_BUCKET}/${S3_PATH}/${INPUT_SOURCE_NAME}" "${INPUT_DEST_NAME}"
+# Configuration
+DST_DIR="input"
+
+# Get the last file matching a prefix
+get_file_by_prefix () {
+  local prefix="$1"
+  echo "Get the last file matching the prefix '${prefix}'…"
+
+  local file_name=$(mc ls "bucket/${S3_BUCKET}/${S3_PATH}/${prefix}" | awk '{ print $NF }' | sort | tail -n1)
+  if [ -z "${file_name}" ]; then
+    echo "ERROR: No input file was found that has this prefix '${prefix}'" >&2
+    exit 1
+  fi
+
+  echo "Copy the file '${file_name}' to '${DST_DIR}/${prefix}.json'…"
+  mc cp "bucket/${S3_BUCKET}/${S3_PATH}/${file_name}" "${DST_DIR}/${prefix}.json"
+  echo "Done!\n"
+}
+
+# Iterator over each file prefix (defined in `file_prefix.sh`)
+OLD_IFS=$IFS
+IFS=","
+for prefix in $FILE_PREFIX; do
+  get_file_by_prefix "${prefix}"
+done
+IFS=$OLD_IFS
 
 exit 0
---
+++ scripts/file_prefix.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Configuration
+FILE_PREFIX="StABS_records,StABS_descriptors"
+
+# Export the $FILE_PREFIX variable, so that it can be used in other scripts
+export FILE_PREFIX
