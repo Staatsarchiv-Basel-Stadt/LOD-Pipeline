e9eea3968a4b48faefc7dc77058ec5f46d64b33d
Ludovic Muller <ludovicmuller1@gmail.com>
2023-12-05 16:23:52 +0100

    ci: work on splitter

--- scripts/splitter.sh
+++ scripts/splitter.sh
@@ -4,73 +4,53 @@
 set -eu
 
 # Configuration
-INPUT_FILE_NAME="input/input.json"
+INPUT_DIR="input"
 SLICE_SIZE=10000
-TMP_FILE="output/tmp_input_split.tmp"
+DST_DIR="output"
 
-# Display the current configuration
-date
-echo "Splitting the '${INPUT_FILE_NAME}' file into files with ${SLICE_SIZE} entries maximum…"
+# Update the file to have one entry per line
+generate_one_entry_per_line () {
+  local file_name="$1"
 
-#  Create the `output` directory and remove old splitted files
-mkdir -p output
-rm -f "${TMP_FILE}"
-rm -f output/raw-split-*.json
-rm -f output/split-*.json
+  jq -cM '.[]' "${file_name}" > "${file_name}.tmp"
+  rm -f "${file_name}"
+  mv "${file_name}.tmp" "${file_name}"
+}
 
-# Generate a temporary file to have raw JSON lines (one line per entry)
-echo "Generate a temporary file, with one entry per line…"
-jq -cM '.[]' "${INPUT_FILE_NAME}" > "${TMP_FILE}"
-echo "Done!"
+# Split the file into multiple files
+split_file () {
+  local file_name="$1"
 
-echo ""
+  # Extract the filename and extension
+  local name=$(basename "$file_name")
+  local extension="${file_name##*.}"
+  local name="${filename%.*}"
 
-# Iterate over each line to generate a file every `$SLICE_SIZE` lines
-# `split` cannot be used, as it is platform-dependant
-date
-echo "Generate the files for the splits…"
-SPLIT_ID=0
-LINE_NB=0
-cat "${TMP_FILE}" | while read -r line; do
-  LINE_NB=$((LINE_NB +  1))
-  OUTPUT_NAME="output/raw-split-${SPLIT_ID}.json"
+  split -d -a 4 -l 10000 "$file_name" "${DST}/SPLITTED-${name}."
+}
 
-  # Handle the creation of a new split file
-  if [ "${LINE_NB}" -eq 1 ]; then
-    SPLIT_ID=$((SPLIT_ID +  1))
-    OUTPUT_NAME="output/raw-split-${SPLIT_ID}.json"
-    echo "\n - Create the '${OUTPUT_NAME}' file for the split…"
-    rm -f "${OUTPUT_NAME}"
-  fi
+# Remove the destination directory and create it again
+rm -rf "${DST_DIR}"
+mkdir -p "${DST_DIR}"
 
-  # Show some progress
-  if [ "$((LINE_NB % 100))" -eq 0 ]; then
-    printf "."
-  fi
-
-  # Print the line in the file
-  printf "%s\n" "${line}" >> "${OUTPUT_NAME}"
-
-  # Reset the line count if we hit the slice size
-  if [ "${LINE_NB}" -eq "${SLICE_SIZE}" ]; then
-    LINE_NB=0
-  fi
+# Iterate over each file to split them
+echo "$(date) - Splitting the files…"
+for file in $INPUT_DIR/*.json; do
+  echo "- $file"
+  generate_one_entry_per_line "$file"
+  split_file "$file"
 done
-echo ""
 
-# Remove the temporary file that we needed
-rm -f "${TMP_FILE}"
-echo ""
-
-# Do some cleanup for the files
-date
-echo "Some cleanup…"
-for file in output/raw-split-*.json; do
-  NEW_NAME=$(echo "${file}" | sed 's/output\/raw-/output\//')
+# Iterate over each splitted file to generate a new file with fixed JSON
+echo "$(date) - Generating the new files…"
+for file in $DST_DIR/SPLITTED-*.json | sort -u; do
+  NEW_NAME=$(echo "${file}" | sed 's/SPLITTED-//')
   rm -f "${NEW_NAME}"
   echo "- $file -> ${NEW_NAME}"
   jq -csM '.' "${file}" > "${NEW_NAME}"
   rm -f "${file}"
 done
+
+echo "$(date) - Done!"
 
 exit 0
