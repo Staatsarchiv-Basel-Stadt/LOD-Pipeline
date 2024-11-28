de776692ba9afe1005c77d4aeace1833afa5f948
Oliver Schihin <oliver.schihin@bs.ch>
2024-11-28 10:30:17 +0100

    Entfernt Prefix wegen OOM-Error

--- scripts/file_prefix.sh
+++ scripts/file_prefix.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Configuration
-FILE_PREFIX="StABS_records,StABS_descriptors,StABS_files"
+FILE_PREFIX="StABS_records,StABS_descriptors"
 
 # Export the $FILE_PREFIX variable, so that it can be used in other scripts
 export FILE_PREFIX
