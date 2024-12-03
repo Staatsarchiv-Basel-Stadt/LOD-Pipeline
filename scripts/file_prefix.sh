01d51911668c433fcc8a622def6786b7205a6bce
Oliver Schihin <oliver.schihin@bs.ch>
2024-12-03 11:39:59 +0100

    Erlaube `file`-Objects

--- scripts/file_prefix.sh
+++ scripts/file_prefix.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Configuration
-FILE_PREFIX="StABS_records,StABS_descriptors"
+FILE_PREFIX="StABS_records,StABS_descriptors,StABS_files"
 
 # Export the $FILE_PREFIX variable, so that it can be used in other scripts
 export FILE_PREFIX
